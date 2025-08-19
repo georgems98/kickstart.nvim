239faa9d2441973b6d55ad2a49ac30a183ad456a
george <gmolinastubbs@gmail.com>
2025-08-19 20:55:15 +0100

    feat: spell check on

--- init.lua
+++ init.lua
@@ -12,10 +12,7 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
@@ -74,6 +71,10 @@ vim.opt.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.opt.confirm = true
+
+-- Spell check
+vim.opt.spell = true
+vim.opt.spelllang = 'en_gb'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
